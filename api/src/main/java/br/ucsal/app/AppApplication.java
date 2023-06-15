6d4e3b451089e2c7feede9c15da3bfe00b3c6d67
Paulo Vitor <paulo19032004@gmail.com>
2023-06-15 20:05:58 -0300

    Config CORS 2

--- api/src/main/java/br/ucsal/app/AppApplication.java
+++ api/src/main/java/br/ucsal/app/AppApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 
 public class AppApplication {
 
-	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
